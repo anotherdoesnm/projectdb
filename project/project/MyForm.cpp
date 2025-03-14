e8dcf295e93ec86921a342dc1a23e88d4c0383d3
Nick <127136860+xomstor@users.noreply.github.com>
2025-03-14 19:28:08 +0300

    ryjgjxrb

--- project/project/MyForm.cpp
+++ project/project/MyForm.cpp
@@ -5,11 +5,11 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-int main(array<String^>^ args) // Change void to int
+int main(array<String^>^ args)
 {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     project::MyForm form;
     Application::Run(% form);
-    return 0; // Return an integer value
+    return 0; 
 }
