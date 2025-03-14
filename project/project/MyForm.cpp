3afd5d5c230f6c08a1a4fd56212b2e1ae811da88
Nick <127136860+xomstor@users.noreply.github.com>
2025-03-14 19:15:00 +0300

    MyForm -> void into INT

--- project/project/MyForm.cpp
+++ project/project/MyForm.cpp
@@ -3,12 +3,13 @@
 
 using namespace System;
 using namespace System::Windows::Forms;
-[STAThread]
 
-void main(array<String^>^ args)
+[STAThread]
+int main(array<String^>^ args) // Change void to int
 {
-	Application::EnableVisualStyles();
-	Application::SetCompatibleTextRenderingDefault(false);
-	project::MyForm form;
-	Application::Run(% form);
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+    project::MyForm form;
+    Application::Run(% form);
+    return 0; // Return an integer value
 }
