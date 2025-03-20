495a129849422bc119983cf01faa06c62f88a94c
Nick <127136860+xomstor@users.noreply.github.com>
2025-03-20 19:32:55 +0300

    оно живое!!!!!!!!!!!!!!!!!!!!!!!

--- test/mainwindow.cpp
+++ test/mainwindow.cpp
@@ -1,15 +1,15 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QFile>
-//#include <QtUiTools>
-//#include <QMessageBox>
+// #include <QtUiTools>
+// #include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent),
     ui(new Ui::MainWindow) {
     setWindowTitle("Test");
     resize(1000,1000);
-    ui->setupUi(parent);
+    ui->setupUi(this);
 }
 
 MainWindow::~MainWindow() {}
--- test/mainwindow.h
+++ test/mainwindow.h
@@ -3,12 +3,21 @@
 
 #include <QMainWindow>
 
+namespace Ui {
+class MainWindow;
+}
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+    private:
+        Ui::MainWindow *ui;
 };
+
+
 
 #endif // MAINWINDOW_H
